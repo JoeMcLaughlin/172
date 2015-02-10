984972a03fced69042a19664d2622033e1144e47
JoeMcLaughlin <jomclis@gmail.com>
2015-02-10 15:06:59 -0800

    Adds all files

---
+++ atwo/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+<!--
+    [x]Start with an empty web site.
+    [x]Add a web form. 
+    [x]Add the ADO Entites.
+    [x]Use the Sql Server Login BasicShowTimeLogin with the password="P@ssw0rd1". 
+    [x]Make sure to include the tables Artist, Show, and ShowDetails. 
+    [x]You should also include Venue.
+    [x]Add a drop down list that shows all the artists in the database. 
+    [x]You will also need the artistkey assigned to the list as a DataValue. 
+    You will need it to link the show to the artist.
+    When the user select an artist the page should show all the shows and show details for that artist.
+  -->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="true" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" ></asp:DropDownList>
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ atwo/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    ShowTrackerEntities showentities = new ShowTrackerEntities();
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            var artists = from Artist in showentities.Artists
+                          orderby Artist.ArtistName
+                          select new { Artist.ArtistName, Artist.ArtistKey };
+            DropDownList1.DataSource = artists.ToList();
+            DropDownList1.DataTextField = "ArtistName";
+            DropDownList1.DataValueField = "ArtistKey";
+            DropDownList1.DataBind();
+        }
+        
+    }
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var show = from Show in showentities.Shows
+                  from Artist in showentities.Artists
+                  from ShowDetail in showentities.ShowDetails
+                  where Artist.ArtistName == DropDownList1.SelectedItem.Text
+                     select new {Show.ShowName, ShowDetail.ShowDetailArtistStartTime };
+        GridView1.DataSource = show.ToList();
+        GridView1.DataBind();
+
+
+        
+    }
+}
